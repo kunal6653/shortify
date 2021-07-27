d07fc1b4b82d1f459935abdad444ff23acacbb04
kunal6653 <82654868+kunal6653@users.noreply.github.com>
2021-07-28 01:54:14 +0530

    Link Shortener Front end

---
+++ text.js
@@ -0,0 +1,18 @@
+function fun(){
+    var txt=document.getElementById('hid1');
+    var btn=document.getElementById('hid2');
+    
+    txt.style.display='block';
+    btn.style.display='block';
+}
+
+function change(){
+  var btn1=document.getElementById('hid2');  
+  btn1.innerHTML='Copied !';
+    
+}
+
+
+
+
+
